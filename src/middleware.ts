00b4fd748d25b7b2433a1674dba9a9fd5f8344a6
Jose G. <jose.garcia26@utp.ac.pa>
2024-11-25 21:59:18 -0500

    feat: protegiendo rutas

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,25 +7,15 @@ export async function middleware(req: NextRequest) {
   console.log('token', token)
 
   if (!token) return NextResponse.redirect(new URL('/login', req.url))
-  // try {
-  //   const user = await payload.verifyEmail({
-  //     collection: 'pacientes',
-  //     token,
-  //   })
-
-  //   if (user) {
-  //     console.log('usuario verificado')
-  //     return NextResponse.redirect(new URL('/', req.url))
-  //   }
-  // } catch (error) {
-  //   console.error('Error de verificación del token:', error)
-  //   return NextResponse.redirect(new URL('/login', req.url))
-  // }
-
-  // // Por defecto, redirige al login si algo falla
-  // return NextResponse.redirect(new URL('/login', req.url))
 }
 
 export const config = {
-  matcher: ['/'],
+  matcher: [
+    '/dashboard/:path*',
+    '/doctor-info/:path*',
+    '/especialidades/:path*',
+    '/historial-de-citas/:path*',
+    '/profileuser/:path*',
+    '/ver-especialistas/:path*',
+  ],
 }
